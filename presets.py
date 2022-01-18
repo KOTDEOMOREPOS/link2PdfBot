d143ada50f1bd3ef2bdd2dad46aa6f5d191d181d
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2022-01-18 16:04:06 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,6 +3,6 @@ class Presets(object):
     PROCESS_TXT = "<code>Pʀᴏᴄᴇssɪɴɢ Yᴏᴜʀ ʟɪɴᴋ..</code>"
     INVALID_LINK_TXT = "<b>Iɴᴠᴀʟɪᴅ Lɪɴᴋ</b>\n\n<i>Pʟᴇᴀsᴇ Sᴇɴᴅ Mᴇ ᴀ Vᴀʟɪᴅ lɪɴᴋ</i>"
     UPLOAD_TXT = "<b>Uᴘʟᴏᴀᴅɪɴɢ Yᴏᴜʀ Fɪʟᴇ..</b>"
-    ERROR_TXT = "<b>URL Error</b>\n\n<i>Unable to create a Pdf with this URL.\nTry again with a valid one..</i>"
-    CAPTION_TXT = "{}\n\n<b>Credits:</b><a href='https://github.com/m4mallu'><b> @M4Mallu</b></a>"
+    ERROR_TXT = "<b>URL Eʀʀᴏʀ</b>\n\n<i>Uɴᴀʙʟᴇ ᴛᴏ ᴄʀᴇᴀᴛᴇ ᴀ Pᴅғ ᴡɪᴛʜ ᴛʜɪs URL.\nTʀʏ Aɢᴀɪɴ Wɪᴛʜ ᴀ Vᴀʟɪᴅ Oɴᴇ..</i>"
+    CAPTION_TXT = "{}\n\n<b>KOT BOTS:</b><a href='https://t.me/KOT_BOTS'><b>KOT BOTS</b></a>"
     THUMB_URL = "https://telegra.ph/file/60706bd59c0829ed2f76f.jpg"
