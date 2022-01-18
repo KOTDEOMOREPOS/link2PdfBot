460c10f0282b49265c49d67bc5daf54acf742524
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2022-01-18 15:58:21 +0530

    Kot

--- presets.py
+++ presets.py
@@ -1,7 +1,7 @@
 class Presets(object):
     START_TXT = "<b>Hᴇʟʟᴏ.. {}</b> 👋\n\n<i>I Aᴍ Sɪᴍᴘʟᴇ Lɪɴᴋ Tᴏ PDF Bᴏᴛ I Cᴀɴ Cᴏɴᴠᴇʀᴛ Tʜᴇ Lɪɴᴋ Iɴᴛᴏ PDF Sᴇɴᴅ Mᴇ Aɴʏ Vᴀʟɪᴅ Lɪɴᴋ Tᴏ Cᴏɴᴠᴇʀᴛ Tᴏ Pᴅғ</i>"
     PROCESS_TXT = "<code>Pʀᴏᴄᴇssɪɴɢ Yᴏᴜʀ ʟɪɴᴋ..</code>"
-    INVALID_LINK_TXT = "<b>Invalid link</b>\n\n<i>Please send me a valid link</i>"
+    INVALID_LINK_TXT = "<b>Iɴᴠᴀʟɪᴅ Lɪɴᴋ</b>\n\n<i>Please send me a valid link</i>"
     UPLOAD_TXT = "<b>Uploading your file..</b>"
     ERROR_TXT = "<b>URL Error</b>\n\n<i>Unable to create a Pdf with this URL.\nTry again with a valid one..</i>"
     CAPTION_TXT = "{}\n\n<b>Credits:</b><a href='https://github.com/m4mallu'><b> @M4Mallu</b></a>"
