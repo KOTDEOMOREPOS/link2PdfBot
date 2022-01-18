a3469ee1d0968071742b5bf3f6bae0feb1acc8cd
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2022-01-18 16:00:22 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,8 +1,8 @@
 class Presets(object):
     START_TXT = "<b>Hᴇʟʟᴏ.. {}</b> 👋\n\n<i>I Aᴍ Sɪᴍᴘʟᴇ Lɪɴᴋ Tᴏ PDF Bᴏᴛ I Cᴀɴ Cᴏɴᴠᴇʀᴛ Tʜᴇ Lɪɴᴋ Iɴᴛᴏ PDF Sᴇɴᴅ Mᴇ Aɴʏ Vᴀʟɪᴅ Lɪɴᴋ Tᴏ Cᴏɴᴠᴇʀᴛ Tᴏ Pᴅғ</i>"
     PROCESS_TXT = "<code>Pʀᴏᴄᴇssɪɴɢ Yᴏᴜʀ ʟɪɴᴋ..</code>"
-    INVALID_LINK_TXT = "<b>Iɴᴠᴀʟɪᴅ Lɪɴᴋ</b>\n\n<i>Please send me a valid link</i>"
-    UPLOAD_TXT = "<b>Uploading your file..</b>"
+    INVALID_LINK_TXT = "<b>Iɴᴠᴀʟɪᴅ Lɪɴᴋ</b>\n\n<i>Pʟᴇᴀsᴇ Sᴇɴᴅ Mᴇ ᴀ Vᴀʟɪᴅ lɪɴᴋ</i>"
+    UPLOAD_TXT = "<b>Uᴘʟᴏᴀᴅɪɴɢ Yᴏᴜʀ Fɪʟᴇ..</b>"
     ERROR_TXT = "<b>URL Error</b>\n\n<i>Unable to create a Pdf with this URL.\nTry again with a valid one..</i>"
     CAPTION_TXT = "{}\n\n<b>Credits:</b><a href='https://github.com/m4mallu'><b> @M4Mallu</b></a>"
     THUMB_URL = "https://telegra.ph/file/60706bd59c0829ed2f76f.jpg"
